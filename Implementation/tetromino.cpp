f7a24a51fa63c633fe14f385aeff0b93cbecef2e
chanhohan <chhqm2759@naver.com>
2021-04-10 15:47:27 +0900

    [ADD] gold5 14500 tetromino

---
+++ Implementation/tetromino.cpp
@@ -0,0 +1,97 @@
+//14500
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+int N, M;
+int g_map[501][501];
+int tmp[501][501];
+int ans;
+
+void input_faster()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+	std::cout.tie(0);
+}
+
+void input()
+{
+	input_faster();
+	std::cin >> N >> M;
+	for (int i = 0 ; i < N ; i++)
+		for (int j = 0 ; j < M ; j++)
+			std::cin >> g_map[i][j];
+}
+
+__inline void max_check(int obj)
+{
+	if (obj > ans)
+		ans = obj;
+}
+
+void check(int i, int j)
+{
+	int obj;
+
+	if (j <= M - 4)
+	{
+		obj = g_map[i][j + 3] + g_map[i][j + 2] + g_map[i][j + 1] + g_map[i][j];
+		max_check(obj);
+	}
+	if (i <= N - 2 && j <= M - 2)
+	{
+		obj = g_map[i][j] + g_map[i][j + 1] + g_map[i + 1][j] + g_map[i + 1][j + 1];
+		max_check(obj);
+	}
+	if (i <= N - 3 && j <= M - 2)
+	{
+		obj = g_map[i][j] + g_map[i + 1][j] + g_map[i + 2][j] + g_map[i + 2][j + 1];
+		max_check(obj);
+		obj = g_map[i][j] + g_map[i + 1][j] + g_map[i + 1][j + 1] + g_map[i + 2][j + 1];
+		max_check(obj);
+		obj = g_map[i][j + 1] + g_map[i + 1][j + 1] + g_map[i + 2][j + 1] + g_map[i + 2][j];
+		max_check(obj);
+		obj = g_map[i][j + 1] + g_map[i + 1][j + 1] + g_map[i + 1][j] + g_map[i + 2][j];
+		max_check(obj);
+	}
+	if (i <= N - 2 && j <= M - 3)
+	{
+		obj = g_map[i][j] + g_map[i][j + 1] + g_map[i][j + 2] + g_map[i + 1][j + 1];
+		max_check(obj);
+	}
+}
+
+void rotate_map()
+{
+	for (int i = 0 ; i < N ; i++)
+		for (int j = 0 ; j < M ; j++)
+			tmp[M - 1 - j][i] = g_map[i][j];
+	std::swap(N, M);
+}
+
+void solve()
+{
+	for (int k = 0 ; k < 4 ; k++)
+	{
+		for (int i = 0 ; i < N ; i++)
+			for (int j = 0 ; j < M ;j++)
+				check(i, j);
+		rotate_map();
+		std::memcpy(g_map, tmp, sizeof(g_map));
+	}
+}
+
+void print_val()
+{
+	std::cout << ans;
+}
+
+int main()
+{
+	input();
+	solve();
+	print_val();
+	return (0);
+}
+
