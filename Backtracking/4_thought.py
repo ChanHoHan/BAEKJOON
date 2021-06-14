905474bc4338cb801e4bdb5e3257f0d1c83ce2b8
chanhohan <chhqm2759@naver.com>
2021-06-14 15:45:11 +0900

    [ADD] silver2 10487 4_thought.py

---
+++ Backtracking/4_thought.py
@@ -0,0 +1,34 @@
+#10487
+import sys
+
+m = int(sys.stdin.readline())
+op = ['+', '-', '*', '//']
+ans = ""
+
+def cal(n, depth, val):
+    global ans
+
+    if (depth == 3):
+        if (n == eval(val)):
+            ans = val
+            return (1)
+        return (0)
+    if(cal(n, depth + 1, val + op[0] + '4') or cal(n, depth + 1, val + op[1] + '4') or
+    cal(n, depth + 1, val + op[2] + '4') or cal(n, depth + 1, val + op[3] + '4')):
+        return (1)
+    return (0)
+
+
+
+for _ in range(m):
+    n = int(sys.stdin.readline())
+    if (cal(n, 0, '4')):
+        i = 0
+        while (i < len(ans)):
+            print(ans[i], end=" ")
+            if (ans[i] == '/'):
+                i += 1
+            i+=1
+        print("=", n)
+    else:
+        print("no solution")
