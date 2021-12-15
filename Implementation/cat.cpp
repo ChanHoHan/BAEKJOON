6a18303881e620900f5d70444e30cf47ff8534bd
chanho <chhqm2759@naver.com>
2021-12-15 16:25:29 +0900

    [ADD] bronze5 10171 cat.cpp

---
+++ Implementation/cat.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define endl "\n"
+void solve()
+
+{
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+}
+
+int main()
+{
+	solve();
+	return (0);
+}
+
