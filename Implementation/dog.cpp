e2260417762acc599a90b6b2deb99fd69721aa50
chanho <chhqm2759@naver.com>
2021-12-15 16:28:51 +0900

    [ADD] bronze5 10172 dog.cpp

---
+++ Implementation/dog.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define endl "\n"
+
+void solve()
+{
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|";
+}
+
+int main()
+{
+	solve();
+	return (0);
+}
+
