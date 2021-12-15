bb718fabf277dc9e72b2f8613bbd6f875eae86dd
chanho <chhqm2759@naver.com>
2021-12-15 15:50:15 +0900

    [ADD] bronze3 2438 counting_stars_1.cpp

---
+++ Implementation/counting_stars_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define endl "\n"
+int N;
+
+void input()
+{
+	FASTIO;
+	cin >> N;
+}
+
+void solve()
+{
+	for (int i = 0 ; i < N ; i++)
+	{
+		for (int j = 0 ; j <= i ; j++)
+			cout << "*";
+		cout << endl;
+	}
+
+}
+
+int main()
+{
+	input();
+	solve();
+	return (0);
+}
